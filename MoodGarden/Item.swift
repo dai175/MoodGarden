4e68ccf75a74785ee3902dfeb71e820f7995028f
Daisuke Oba <dai175dai@gmail.com>
2026-03-17 21:05:22 +0900

    Initial Commit

---
+++ MoodGarden/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MoodGarden
+//
+//  Created by Daisuke Ooba on 2026/03/17.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MoodGardenTests/MoodGardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodGardenTests.swift
+//  MoodGardenTests
+//
+//  Created by Daisuke Ooba on 2026/03/17.
+//
+
+import Testing
+@testable import MoodGarden
+
+struct MoodGardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
